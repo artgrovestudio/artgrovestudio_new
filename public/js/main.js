09fc4be5809daad71eb3ad88df2a214a38fa69ad
artgrovestudio <zizu19988@gmail.com>
2017-02-25 04:49:47 +0300

    change slide structure

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,26 @@
 window.onload = function () {
   var loader = $('.loader');
   var logo = $('.nav .logo');
+  var hamburger = $('.nav .hamburger');
+  var hamburgerLine = $('.nav .hamburger .line');
   var tl = new TimelineMax();
 
-  tl.to(loader, 1, {opacity: "0", ease:Power2.easeInOut}, 2)
-  .to(loader, 0, {zIndex: "-99999"})
-  .to(logo, 1, {left: "0", transform: "translateX(0)", ease:Power4.easeInOut}, 2)
+  tl.to(loader, .5, {opacity: "0", ease:Power2.easeInOut}, 2) // Opacity for loader
+  .to(loader, 0, {zIndex: "-99999"}) // Z-Index for loader
+  .to(hamburger, 0, {display: "block"}, -1) // Display for hamburger
+  .to(logo, 1, {left: "0", transform: "translateX(0)", ease:Power4.easeOut}, 2) // Animation logo
+  .to(hamburgerLine, .5, {opacity: "1", ease:Power4.easeInOut}, 2) // Opacity for hamburger
+
+  // Nav desktop
+
+	$('.nav .hamburger').click(function(){
+		if ($(".nav .hamburger").hasClass("hamburger-open")) {
+			$(".nav .hamburger").removeClass("hamburger-open");
+			$(".nav").removeClass("nav-desktop-open");
+		}
+		else {
+			$(".nav .hamburger").addClass("hamburger-open");
+			$(".nav").addClass("nav-open");
+		}
+	});
 }
